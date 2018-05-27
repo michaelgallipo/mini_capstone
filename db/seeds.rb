437adecc7a2f45c17f80564a2cdcec84f0d52e26
michael <michael.gallipo@gmail.com>
2018-05-27 17:17:33 -0400

    started to add products to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+helmet = Product.create(name: "Cascde S", category: "Protective", price: 259, description: "Casacade's latest and greatest helmet", color: "Matte Black", image_url: "")
+stick = Product.create(name: "Warrior EVO Warp Next", category: "Stick", price: 99, description: "This lacrosse stick is designed to teach new players how to catch and throw better with amazing consistency.", color: "White", image_url: "")
+pads = Product.create(name: "Nike Vapor 2.0 Shoulder Pads", category: "Protective", price: 129, description: "These are attackman and offensive middie pads that have a softer feel but maintain a snug fit", color: "Gray", image_url: "")
+
+
