d394d28580516c9ffea47f86639ffcf8928170c4
michael <michael.gallipo@gmail.com>
2018-06-15 11:29:14 -0400

    Added ProductCategory join table and relationships

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,12 @@
 class Product < ApplicationRecord
 
   belongs_to :supplier
+  
 
   has_many :images
   has_many :orders
+  has_many :product_categories
+  has_many :categories, through: :product_categories
 
   def is_discounted?
     price <100
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_13_041259) do
+ActiveRecord::Schema.define(version: 2018_06_15_043854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "images", force: :cascade do |t|
     t.string "url"
@@ -33,9 +39,15 @@ ActiveRecord::Schema.define(version: 2018_06_13_041259) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "product_categories", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.string "category"
     t.decimal "price", precision: 5, scale: 2
     t.text "description"
     t.string "color"
