7733459b6ee77700ef31be630677fcfba2524dad
michael <michael.gallipo@gmail.com>
2018-07-11 21:36:41 -0400

    Added addl VueJS functionality

--- public/index.js
+++ public/index.js
@@ -1,24 +1,170 @@
-var productTemplate = document.querySelector('#product-card');
-var productRow = document.querySelector(".row");
+/* global Vue, VueRouter, axios */
 
-/* global sxios */
-axios.get("http://localhost:3000/api/products").then(function(response) {
-  var products = response.data;
-  console.log(products);
+var HomePage = {
+  template: "#home-page",
+  data: function() {
+    return {
+      message: "Lacrosse Equipment Emporium",
+      products: []
+    };
+  },
+  created: function() {
+    axios.get("/api/products").then(function(response) {
+      this.products = response.data;
+      console.log(this.products);
+    }.bind(this));
+  },
+  methods: {},
+  computed: {}
+};
+
+var SamplePage = {
+  template: "#sample-page",
+  data: function() {
+    return {
+      message: "Lacrosse Equipment Emporium Sample Page"
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
+
+var SignupPage = {
+  template: "#signup-page",
+    data: function() {
+      return {
+        name: "",
+        email: "",
+        password: "",
+        passwordConfirmation: "",
+        errors: []
+      };
+    },
+  methods: {
+    submit: function() {
+      var params = {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+        password_confirmation: this.passwordConfirmation
+      };
+      axios
+        .post("/users", params)
+        .then(function(response) {
+          router.push("/login");
+        })
+        .catch(
+          function(error) {
+            this.errors = error.response.data.errors;
+          }.bind(this)
+        );
+    }
+  }
+};
+
+var ProductNewPage = {
+  template: "#product-new-page",
+    data: function() {
+      return {
+        name: "",
+        price: 0.00,
+        description: "",
+        color: "",
+        availability: true,
+        supplier_id: "",
+        errors: []
+      };
+    },
+  methods: {
+    submit: function() {
+      var params = {
+        name: this.name,
+        price: this.price,
+        description: this.description,
+        color: this.color,
+        availability: this.availability,
+        supplier_id: this.supplier_id
+      };
+      axios
+        .post("/api/products", params)
+        .then(function(response) {
+          router.push("/login");
+        })
+        .catch(
+          function(error) {
+            this.errors = error.response.data.errors;
+          }.bind(this)
+        );
+    }
+  }
+};
 
 
-  products.forEach(function(product) {
-    var productClone = productTemplate.content.cloneNode(true);
-    productClone.querySelector(".card-img-top").src = product.images[0].url;
-    productClone.querySelector(".card-title").innerText = product.name;
-    productClone.querySelector(".price").innerText = `Price: ${ product.price }`;
-    productClone.querySelector(".description").innerText = product.description;
-    productClone.querySelector(".color").innerText = `Color: ${product.color}`;
-    product.categories.forEach(function(category) {
-      productClone.querySelector(".category").innerText = category.name;      
-    })
+var LoginPage = {
+  template: "#login-page",
+  data: function() {
+    return {
+      email: "",
+      password: "",
+      errors: []
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        auth: { email: this.email, password: this.password }
+      };
+      axios
+        .post("/user_token", params)
+        .then(function(response) {
+          axios.defaults.headers.common["Authorization"] =
+            "Bearer " + response.data.jwt;
+          localStorage.setItem("jwt", response.data.jwt);
+          router.push("/");
+        })
+        .catch(
+          function(error) {
+            this.errors = ["Invalid email or password."];
+            this.email = "";
+            this.password = "";
+          }.bind(this)
+        );
+    }
+  }
+};
 
-    productRow.appendChild(productClone);
-  });
+var LogoutPage = {
+  template: "<h1>Logout</h1>",
+  created: function() {
+    axios.defaults.headers.common["Authorization"] = undefined;
+    localStorage.removeItem("jwt");
+    router.push("/");
+  }
+};
 
-})
+
+var router = new VueRouter({
+  routes: [
+  { path: "/", component: HomePage },
+  { path: "/sample", component: SamplePage},
+  { path: "/signup", component: SignupPage },
+  { path: "/login", component: LoginPage },
+  { path: "/logout", component: LogoutPage },
+  { path: "/products/new", component: ProductNewPage}],
+  scrollBehavior: function(to, from, savedPosition) {
+    return { x: 0, y: 0 };
+  }
+});
+
+var app = new Vue({
+  el: "#vue-app",
+  router: router,
+  created: function() {
+    var jwt = localStorage.getItem("jwt");
+    if (jwt) {
+      axios.defaults.headers.common["Authorization"] = jwt;
+    }
+  }
+
+});
